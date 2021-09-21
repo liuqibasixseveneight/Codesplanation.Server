9498ca830bcb3ff2c6cd23164ce5215029b37418
liuqibasixseveneight <liuqiba.sixseveneight@gmail.com>
2021-09-21 22:54:24 +0100

    INIT: Adding initial Codesplanation Server

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,18 @@
+const commentsResolvers = require("./comments");
+const postsResolvers = require("./posts");
+const usersResolvers = require("./users");
+
+module.exports = {
+  Post: {
+    commentCount: (parent) => parent.comments.length,
+    likeCount: (parent) => parent.likes.length,
+  },
+  Query: {
+    ...postsResolvers.Query,
+  },
+  Mutation: {
+    ...commentsResolvers.Mutation,
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+  },
+};
