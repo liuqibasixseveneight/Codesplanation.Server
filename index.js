79cef270b3cca3ebca16c766980519d9302dc9db
liuqiba <85908203+liuqibasixseveneight@users.noreply.github.com>
2022-04-30 21:28:32 +0100

    fix: Rolling back apollo-server  version (#9)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB_CONNECTION_STRING } = require('./config');
+
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
   typeDefs,
